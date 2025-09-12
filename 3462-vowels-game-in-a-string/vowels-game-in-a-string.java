79f68efa8c337e52d5140d7803ae094e4461feb0
VIKAS MAURYA <117437513+vikas9616@users.noreply.github.com>
2025-09-12 18:09:05 +0530

    Time: 4 ms (79.79%) | Memory: 45.8 MB (30.18%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        if(s.contains("a")||s.contains("e")||s.contains("i")||s.contains("o")||s.contains("u"))     
+        return true;
+        return false;
+    }
+}

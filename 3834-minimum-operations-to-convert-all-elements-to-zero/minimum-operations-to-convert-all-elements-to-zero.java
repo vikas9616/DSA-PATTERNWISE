b992a2d97acbfe08ac93d426458a4290b12e20aa
VIKAS MAURYA <117437513+vikas9616@users.noreply.github.com>
2025-11-10 18:46:43 +0530

    Time: 34 ms (69.01%) | Memory: 123.4 MB (12.68%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans=0;
+        Deque<Integer> stack=new ArrayDeque<>();
+        stack.push(0);
+
+        for(int num:nums){
+            while(!stack.isEmpty() && stack.peek()>num){
+                stack.pop();
+            }
+            if(stack.isEmpty() || stack.peek()<num){
+                ans++;
+                stack.push(num);
+            }
+        }
+        return ans;
+        
+    }
+}
